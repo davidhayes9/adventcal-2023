d2bfd408fb18f4edd3ebfeacdbf4ba874a065ef0
dhayes <dhayes@arista.com>
2023-12-06 18:34:31 +1100

    day6 complete

--- day6/day6.py
+++ day6/day6.py
@@ -27,13 +27,13 @@ def str_to_list(ls):
 def win_race(t, d):
     wins = 0
     for i in range(t+1):
-        if part2 == 0:
+        if part == 1:
             speed = (i * 1)
             distance = (speed * (t - i))
             if distance > d:
                 wins += 1
 
-        if part2 == 1:
+        if part == 2:
             if i >= 14:
                 speed = (i * 1)
                 distance = (speed * (t - i))
@@ -55,12 +55,12 @@ def get_wins(time, dist):
 time = str_to_list(ls_times)
 dist = str_to_list(ls_dist)
 
-part2 = 0
+part = 1
 get_wins(time, dist)
 
 #part 2
 race_time = ls_times.replace( " ", "")
 race_distance = ls_dist.replace( " ", "")
 
-part2 = 1
+part = 2
 print(win_race(int(race_time), int(race_distance)))
